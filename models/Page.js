49a8095e4be5a85147a54ed6eff71029ac7adac7
Nate Hawthorne <natejhawthorne@gmail.com>
2016-05-04 00:46:26 -0500

    Added one to many relationships in models

--- models/Page.js
+++ models/Page.js
@@ -1,8 +1,12 @@
 require('./User');
+require('./Button');
 module.exports = bookshelf.model('Page', {
 	tableName: 'pages',
 	hasTimestamps: ['createdAt', 'updatedAt', 'deletedAt'],
 	user: function() {
 		return this.belongsTo('User', 'userId');
+	},
+	buttons: function() {
+		return this.hasMany('Button', 'buttonId');
 	}
 });
--- models/User.js
+++ models/User.js
@@ -1,8 +1,12 @@
 require('./Authentication');
+require('./Page');
 module.exports = bookshelf.model('User', {
 	tableName: 'users',
 	hasTimestamps: ['createdAt', 'updatedAt', 'deletedAt'],
 	authentication: function() {
 		return this.hasMany('Authentication', 'userId');
+	},
+	pages: function() {
+		return this.hasMany('Page', 'pageId');
 	}
 });
