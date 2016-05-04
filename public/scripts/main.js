171d4aae1464ad6eac24099e24957e133a873c8c
Nate Hawthorne <natejhawthorne@gmail.com>
2016-05-03 22:26:28 -0500

    Added example of Filestack working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,18 @@
 console.log('js working');
 console.log('another js thing');
+
+filepicker.setKey('ABF95lzYQqNV2ewYLYYQyz');
+
+filepicker.pick(
+ {
+    mimetype: 'image/*',
+    container: 'window',
+    services: ['COMPUTER', 'FACEBOOK', 'CLOUDAPP']
+  },
+  function(test){
+    console.log(JSON.stringify(test));
+  },
+  function(FPError){
+ 	console.log(FPError.toString()); //- print errors to console
+  }
+);
