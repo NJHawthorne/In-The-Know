7fdffea3535245533a870623758a89aafdf73e0a
Nate Hawthorne <natejhawthorne@gmail.com>
2016-05-04 10:53:22 -0500

    Created barebone page components

---
+++ public/scripts/components/Pages/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default React.createClass({
+	render: function() {
+		return (
+			<section>
+				<h1>This is the Content page!</h1>
+			</section>
+		);
+	}
+});
---
+++ public/scripts/components/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default React.createClass({
+	render: function() {
+		return (
+			<section>
+				<h1>This is the Home page!</h1>
+			</section>
+		);
+	}
+});
