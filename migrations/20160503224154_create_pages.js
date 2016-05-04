5e778b3f12b5ca3ba5df68302937424297df3063
Nate Hawthorne <natejhawthorne@gmail.com>
2016-05-04 00:23:13 -0500

    Added page model and migration

---
+++ migrations/20160503224154_create_pages.js
@@ -0,0 +1,20 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('pages', function(t) {
+		t.increments('id').unsigned().primary();
+		t.dateTime('createdAt').notNull();
+		t.dateTime('updatedAt').nullable();
+		t.dateTime('deletedAt').nullable();
+
+		t.string('pageName').nullable();
+		t.integer('userId')
+			.unsigned()
+			.notNull()
+			.references('id')
+			.inTable('users')
+			.onDelete('CASCADE');
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTable('pages');
+};
