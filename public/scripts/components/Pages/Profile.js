80af2d0ff3ff18b167a8405b92d0ee3f198196cb
Nate Hawthorne <natejhawthorne@gmail.com>
2016-05-04 14:47:49 -0500

    Bug fixes and router updates

--- public/scripts/components/Pages/Profile.js
+++ public/scripts/components/Pages/Profile.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import BasicInfo from './../Subcomponents/BasicInfo.js';
+import UsersPages from './../Subcomponents/UsersPages.js';
+import AccountSettings from './../Subcomponents/AccountSettings.js';
 
 export default React.createClass({
 	render: function() {
 		return (
 			<section>
 				<h1>This is the Profile page!</h1>
+				<BasicInfo />
+				<UsersPages />
+				<AccountSettings />
 			</section>
 		);
 	}
