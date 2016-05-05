1f9d2448844ac9a4597c78453d4b8fcee0b408fa
Nate Hawthorne <natejhawthorne@gmail.com>
2016-05-05 11:31:36 -0500

    Made the login page functional

--- public/scripts/components/Pages/Login.js
+++ public/scripts/components/Pages/Login.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import user from './../../models/UserModel';
+import $ from 'jquery';
 
 export default React.createClass({
+	getInitialState: function() {
+		return {
+			user: user,
+			error: {}
+		};
+	},
 	render: function() {
 		return (
 			<section>
-				<h1>This is the Login page!</h1>
+				<form onSubmit={this.handleLogin}>
+					<div 
+						className="error">{this.state.errors.email ? this.state.errors.email.message : null}</div>
+					<input
+						placeholder="Email"
+						ref="email"
+						type="text"
+						required="required" />
+					<div 
+						className="error">{this.state.errors.password ? this.state.errors.password.message : null} </div>
+					<input
+						placeholder="Password"
+						ref="password"
+						type="password"
+						required="required" />
+					<button
+						type="submit">Login</button>
+				</form>
 			</section>
 		);
+	},
+	handleLogin: function(e) {
+		e.preventDefault();
+		console.log('You pressed the new login button!');
+		console.log(this.state);
+		$.ajax({
+			url: 'auth/login',
+			type: 'POST',
+			data: {
+				email: this.refs.email.value,
+				password: this.refs.password.value
+			},
+			headers: {
+				Accept: 'application/json'
+			},
+			success: (login) => {
+				this.state.user.set(login);
+				console.log('IT WORKED! :D');
+			},
+			error: (err) => {
+				this.setState({error: err.responseJSON});
+			}
+		});
 	}
 });
