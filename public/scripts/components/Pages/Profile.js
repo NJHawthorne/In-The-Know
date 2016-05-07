c285cc8dfc92927da775d18455d904bb4746b17d
Nate Hawthorne <natejhawthorne@gmail.com>
2016-05-07 17:46:26 -0500

    Updated seed and got accurate data from it

--- public/scripts/components/Pages/Profile.js
+++ public/scripts/components/Pages/Profile.js
@@ -2,16 +2,38 @@ import React from 'react';
 import BasicInfo from './../Subcomponents/BasicInfo.js';
 import UsersPages from './../Subcomponents/UsersPages.js';
 import AccountSettings from './../Subcomponents/AccountSettings.js';
+import user from './../../models/UserModel';
+import $ from 'jquery';
 
 export default React.createClass({
+	getInitialState: function() {
+		return {
+			user: user,
+			pages: []
+		};
+	},
+	componentDidMount: function() {
+		this.pageRequest = $.get('/api/v1/page', function(data) {
+			for (var i = 0; i < data.length; i++) {
+				if(data[i].userId == this.state.user.get('id')) {
+					this.state.pages.push(data[i].pageName);
+				}
+			}
+			this.setState({pages: this.state.pages});
+		}.bind(this));
+	},
 	render: function() {
-		return (
-			<section>
-				<h1>This is the Profile page!</h1>
-				<BasicInfo />
-				<UsersPages />
-				<AccountSettings />
-			</section>
-		);
+		console.log('render');
+		console.log(this.state);
+		if(this.state.user.get('id') == this.props.params.userId){
+			return (
+				<section>
+					<h1>This is the Profile page!</h1>
+					<BasicInfo />
+					<UsersPages />
+					<AccountSettings />
+				</section>
+			);
+		}
 	}
 });
--- seeds/dev/seed_users.js
+++ seeds/dev/seed_users.js
@@ -75,6 +75,24 @@ exports.seed = function(knex, Promise) {
 					);
 				})
 			);
+		})
+		.then(userId => {
+			userId = userId[0];
+			return Promise.join(
+				knex('pages').insert({ createdAt: date, pageName: 'Game of Thrones', userId: 2})
+				.returning('id')
+				.then(pageId => {
+					pageId = pageId[0];
+					return Promise.join(
+						knex('buttons').insert({ createdAt: date, buttonName: 'Stark', pageId: 3, icon: 'fa-tree', color: 'gray', posTop: 18, posLeft: 5, imageUrl: 'https://40.media.tumblr.com/tumblr_lpoez2k1ap1qhq895o1_500.jpg'}),
+			    		knex('buttons').insert({ createdAt: date, buttonName: 'Lannister', pageId: 3, icon: 'fa-deaf', color: 'gold', posTop: 8, posLeft: 25, imageUrl: 'http://25.media.tumblr.com/tumblr_mbn1dhxNxa1rtug9eo7_1280.jpg'}),
+					    knex('buttons').insert({ createdAt: date, buttonName: 'Targaryen', pageId: 3, icon: 'fa-fire', color: 'red', posTop: 3, posLeft: 7, imageUrl: 'http://winteriscoming.net/wp-content/uploads/2016/02/House-Targ.-Game-of-Thrones-810x569.jpg'}),
+					    knex('buttons').insert({ createdAt: date, buttonName: 'Bolton', pageId: 3, icon: 'fa-male', color: 'black', posTop: 28, posLeft: 16, imageUrl: 'http://vignette2.wikia.nocookie.net/gameofthrones/images/c/cf/House_Bolton_slider_HBO.jpg/revision/latest?cb=20130429043504'}),
+					    knex('buttons').insert({ createdAt: date, buttonName: 'High Sparrow', pageId: 3, icon: 'fa-twitter', color: 'tan', posTop: 12, posLeft: 1, imageUrl: 'http://m0.joe.ie/wp-content/uploads/2015/05/26135623/HighSparrow.jpg'}),
+					    knex('buttons').insert({ createdAt: date, buttonName: 'Dothraki', pageId: 3, icon: 'fa-ship', color: 'brown', posTop: 0, posLeft: 30, imageUrl: 'http://getinmedia.com/sites/default/files/images/Dothraki%20Inline.jpg'})
+					);
+				})
+			);
 		});
 	});
 };
