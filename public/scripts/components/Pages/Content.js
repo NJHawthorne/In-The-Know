39942c148ff0407425fab1adc0c707b30b3b4289
Nate Hawthorne <natejhawthorne@gmail.com>
2016-05-08 11:33:45 -0500

    Successfully got button data from server

--- public/scripts/components/Pages/Content.js
+++ public/scripts/components/Pages/Content.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import buttons from './../../collections/ButtonCollection';
+import $ from 'jquery';
 
 export default React.createClass({
+	getInitialState: function() {
+		return {
+			buttons: buttons,
+			eachButton: []
+		};
+	},
+	componentDidMount: function() {
+		$.ajax({
+			url: '/api/v1/button',
+			method: 'get', 
+			accepts: 'application/json',
+			data: {
+				where: {
+					pageId: 2
+				}
+			},
+			success: (data) => {
+				for (var i = 0; i < data.length; i++) {
+					this.state.eachButton.push({
+						buttonName: data[i].buttonName,
+						color: data[i].color,
+						icon: data[i].icon,
+						imageUrl: data[i].imageUrl,
+						posLeft: data[i].posLeft,
+						posTop: data[i].posTop
+					});
+				}
+				this.setState({eachButton: this.state.eachButton});
+			} 
+		});
+	},
 	render: function() {
+		console.log('render');
+		console.log(this.state);
 		return (
 			<section>
 				<h1>This is the Content page!</h1>
