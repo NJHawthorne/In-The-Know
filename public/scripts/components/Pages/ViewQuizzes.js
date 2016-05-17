fa013b98d207bb7547abfd45feb56fdcaee75f4c
Nate Hawthorne <natejhawthorne@gmail.com>
2016-05-16 22:16:49 -0500

    Updated ViewQuizzes title

--- public/scripts/components/Pages/ViewQuizzes.js
+++ public/scripts/components/Pages/ViewQuizzes.js
@@ -26,7 +26,7 @@ export default React.createClass({
 		});
 		return (
 			<section>
-				<h1 className='viewQuizzes'>YOOOO THIS IS WHERE THE QUIZZES LIVE</h1>
+				<h1 className='viewQuizzes'>Quizzes</h1>
 				{eachQuiz}
 			</section>
 		);
